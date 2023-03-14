3ab2a1233e74c717c4bc34cbfc9f01c170466dd4
MagliariElio <magliari.elio08@gmail.com>
2023-03-14 15:26:37 +0100

    new updates

--- spotify.py
+++ spotify.py
@@ -22,27 +22,62 @@ SCOPE = 'user-read-playback-state user-read-currently-playing'
 username = config['spotify']['username']
 token = util.prompt_for_user_token(username, SCOPE, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
 
-# Creazione dell'oggetto Spotipy
-sp = spotipy.Spotify(auth=token)
 
-song = sp.current_playback()
-if(song['item'] != None):
-    print("\nCanzone: '" + song['item']['name'] + "'\nAlbum: '" + song['item']['album']['name'] + "'\nAutore: '" + song['item']['album']['artists'][0]['name'] + "'\n")
-
-while True:
-    current = sp.current_playback()
+def print_canzone(song):
+    if(song == None or song['item'] == None):
+        return
     
-    if(current['item'] == None):
-        os.popen("pkill spotify")       # chiude il processo
-        os.popen("spotify")             # apre di nuovo il processo
-        #sp.start_playback()            # riprende la canzone in automatico pero' e' una funzione premium
-        time.sleep(10)
+    if(song['item']['name'] != None):
+        print("Canzone: '" + song['item']['name'])
     
-    if(current['item'] != None and current['item']['name'] != song['item']['name']):
-        song = current
-        print("Canzone: '" + song['item']['name'] + "'\nAlbum: '" + song['item']['album']['name'] + "'\nAutore: '" + song['item']['album']['artists'][0]['name'] + "'\n")
+    if(song['item']['album']['name'] != None):
+        print("Album: '" + song['item']['album']['name'])
+    
+    if(song['item']['album']['artists'][0]['name'] != None):
+        print("Autore: '" + song['item']['album']['artists'][0]['name'] + "'\n")
+    
+    return
 
-    time.sleep(3)
 
+def main():
+    # Creazione dell'oggetto Spotipy
+    sp = spotipy.Spotify(auth=token)
 
+    song = sp.current_playback()
 
+    # Nel caso l'applicazione fosse chiusa, viene aperta automaticamente forkando un processo
+    if(song == None):
+        os.popen("spotify")
+        print("Apertura automatica di Spotify")
+
+    if(song != None and song['item'] != None):
+        print_canzone(song)
+
+    while True:
+        current = sp.current_playback()
+        
+        if(current == None or song == None):
+            current = sp.current_playback()
+            song = current
+            print_canzone(song)
+            continue
+        
+        if(current['item'] == None):
+            print("Pubblicità\n")
+            time.sleep(2)    # TODO: da rimuovere
+            os.popen("pkill spotify")       # chiude il processo
+            os.popen("spotify")             # apre di nuovo il processo
+            #sp.start_playback()            # riprende la canzone in automatico pero' e' una funzione premium
+            time.sleep(10)
+        
+        if(current['item'] != None and current['item']['name'] != song['item']['name']):
+            song = current
+            print_canzone(song)
+
+        time.sleep(3)
+
+    return
+
+if(__name__ == '__main__'):
+    main()
+    
